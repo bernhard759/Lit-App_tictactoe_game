37e2fe2d0c9b025e10dfe2c613fa74659a66d585
bernhard759 <bernhard.gailer@gmx.de>
2023-03-09 17:00:55 +0100

    add gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Lit-App_tictactoe_game/",
   build: {
     lib: {
       entry: 'src/my-element.ts',
