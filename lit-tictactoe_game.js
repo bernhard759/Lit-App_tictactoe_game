e330281b55a7e417d80a4788791d4b1d891aaa7d
bernhard759 <bernhard.gailer@gmx.de>
2023-03-09 17:12:24 +0100

    Updates

--- lit-tictactoe_game.js
+++ lit-tictactoe_game.js
@@ -1,6 +1,30 @@
-import { css as d, LitElement as c, html as l } from "lit";
-import { state as a, customElement as u } from "lit/decorators.js";
-const m = d`
+import { css as u, LitElement as h, html as d } from "lit";
+import { state as c, customElement as m } from "lit/decorators.js";
+(function() {
+  const t = document.createElement("link").relList;
+  if (t && t.supports && t.supports("modulepreload"))
+    return;
+  for (const e of document.querySelectorAll('link[rel="modulepreload"]'))
+    s(e);
+  new MutationObserver((e) => {
+    for (const r of e)
+      if (r.type === "childList")
+        for (const a of r.addedNodes)
+          a.tagName === "LINK" && a.rel === "modulepreload" && s(a);
+  }).observe(document, { childList: !0, subtree: !0 });
+  function o(e) {
+    const r = {};
+    return e.integrity && (r.integrity = e.integrity), e.referrerPolicy && (r.referrerPolicy = e.referrerPolicy), e.crossOrigin === "use-credentials" ? r.credentials = "include" : e.crossOrigin === "anonymous" ? r.credentials = "omit" : r.credentials = "same-origin", r;
+  }
+  function s(e) {
+    if (e.ep)
+      return;
+    e.ep = !0;
+    const r = o(e);
+    fetch(e.href, r);
+  }
+})();
+const p = u`
 #gameboard {
     width: 50vmin;
     aspect-ratio: 1;
@@ -47,37 +71,37 @@ const m = d`
     background-color: hsl(120, 30%, 55%) !important;
   }
 `;
-var g = Object.defineProperty, p = Object.getOwnPropertyDescriptor, o = (t, e, s, n) => {
-  for (var r = n > 1 ? void 0 : n ? p(e, s) : e, h = t.length - 1, _; h >= 0; h--)
-    (_ = t[h]) && (r = (n ? _(e, s, r) : _(r)) || r);
-  return n && r && g(e, s, r), r;
+var _ = Object.defineProperty, g = Object.getOwnPropertyDescriptor, l = (i, t, o, s) => {
+  for (var e = s > 1 ? void 0 : s ? g(t, o) : t, r = i.length - 1, a; r >= 0; r--)
+    (a = i[r]) && (e = (s ? a(t, o, e) : a(e)) || e);
+  return s && e && _(t, o, e), e;
 };
-let i = class extends c {
+let n = class extends h {
   constructor() {
     super(...arguments), this._board = new Array(9).fill(""), this._turn = "X", this._gameover = !1, this._winner = "", this._winningCombo = [], this._userAction = !0;
   }
   // Render the markup
   render() {
-    return this._gameover ? l`
+    return this._gameover ? d`
     <div class="gameover">
       ${this._getGameoverMessage()}
       <button @click=${this._playNewGame}>Play again</button>
-    </div>` : l`
+    </div>` : d`
       <div id="gameboard">
         ${this._board.map(
-      (t, e) => l`<div @click=${() => this._handleClickedSquare(e)} class=${this._winningCombo.includes(e) ? "square winner" : "square"}>${t}</div>`
+      (i, t) => d`<div @click=${() => this._handleClickedSquare(t)} class=${this._winningCombo.includes(t) ? "square winner" : "square"}>${i}</div>`
     )}
       </div>`;
   }
   // Get gameover message
   _getGameoverMessage() {
-    let t;
-    return this._winner === "" ? t = l`<div>The game ended in a draw.</div>` : t = l`<div>The winner is ${this._winner}.</div>`, t;
+    let i;
+    return this._winner === "" ? i = d`<div>The game ended in a draw.</div>` : i = d`<div>The winner is ${this._winner}.</div>`, i;
   }
   // Clicked square
-  _handleClickedSquare(t) {
-    const e = [...this._board];
-    e[t] !== "" || !this._userAction || (e[t] = this._turn, this._board = e, this._isWinner(), this._isDraw(), this._turn = this._turn === "X" ? "O" : "X");
+  _handleClickedSquare(i) {
+    const t = [...this._board];
+    t[i] !== "" || !this._userAction || (t[i] = this._turn, this._board = t, this._isWinner(), this._isDraw(), this._turn = this._turn === "X" ? "O" : "X");
   }
   // Check for a winner
   _isWinner() {
@@ -90,42 +114,39 @@ let i = class extends c {
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6]
-    ].forEach((e) => {
-      const [s, n, r] = e;
-      this._board[s] !== "" && this._board[s] === this._board[n] && this._board[s] === this._board[r] && (console.log("winner"), this._userAction = !1, this._winner = this._turn, setTimeout(() => this._gameover = !0, 2e3), this._winningCombo = e);
+    ].forEach((t) => {
+      const [o, s, e] = t;
+      this._board[o] !== "" && this._board[o] === this._board[s] && this._board[o] === this._board[e] && (console.log("winner"), this._userAction = !1, this._winner = this._turn, setTimeout(() => this._gameover = !0, 2e3), this._winningCombo = t);
     }), !1;
   }
   // Check for a draw
   _isDraw() {
-    this._board.filter((e) => e === "").length === 0 && console.log("draw");
+    this._board.filter((t) => t === "").length === 0 && console.log("draw");
   }
   // Play a new game
   _playNewGame() {
     this._board = Array(9).fill(""), this._gameover = !1, this._userAction = !0, this._winningCombo = [];
   }
 };
-i.styles = m;
-o([
-  a()
-], i.prototype, "_board", 2);
-o([
-  a()
-], i.prototype, "_turn", 2);
-o([
-  a()
-], i.prototype, "_gameover", 2);
-o([
-  a()
-], i.prototype, "_winner", 2);
-o([
-  a()
-], i.prototype, "_winningCombo", 2);
-o([
-  a()
-], i.prototype, "_userAction", 2);
-i = o([
-  u("tictactoe-game")
-], i);
-export {
-  i as MyElement
-};
+n.styles = p;
+l([
+  c()
+], n.prototype, "_board", 2);
+l([
+  c()
+], n.prototype, "_turn", 2);
+l([
+  c()
+], n.prototype, "_gameover", 2);
+l([
+  c()
+], n.prototype, "_winner", 2);
+l([
+  c()
+], n.prototype, "_winningCombo", 2);
+l([
+  c()
+], n.prototype, "_userAction", 2);
+n = l([
+  m("tictactoe-game")
+], n);
